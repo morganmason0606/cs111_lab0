1cde6d531f0110decd6564bdea5cde77643569b4
Morgan Mason <morganmason0606@gmail.com>
2023-04-07 11:38:07 -0700

    first attempt at sol, still needs cleaning up

--- proc_count.c
+++ proc_count.c
@@ -4,8 +4,19 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
+static int example(struct seq_file *m, void *v){
+	//seq_printf(m, "hello world\n");//writes to file
+	int i = 0;
+	for_each_process(p){
+		i++;
+	}
+	seq_printf(m,i);
+	return 0;
+}
+
 static int __init proc_count_init(void)
 {
+	entry = proc_create_sinlge("count", 0, NULL, example)//creates a process of that name
 	pr_info("proc_count: init\n");
 	return 0;
 }
