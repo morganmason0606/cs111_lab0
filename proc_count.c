706705286ababbcaad661fa39ad83d440c529e69
Morgan Mason <morganmason0606@gmail.com>
2023-04-08 16:09:15 -0700

    first fixes; turned int into string

--- proc_count.c
+++ proc_count.c
@@ -4,27 +4,43 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
-static struct proc_dir_entry *entry; 
-static int example(struct seq_file *m, void *v){
+#define _OPEN_SYS_ITOA_EXT
+#include <stdlib.h>
+
+static struct proc_dir_entry *entry = NULL;
+
+static int example(struct seq_file *m, void *p){
 	//seq_printf(m, "hello world\n");//writes to file
-	int i = 0;
+	int i = 0; //int here assumes there will be less than INT MAX processes on a computer, this feels like a fair assumption
 	for_each_process(p){
 		i++;
 	}
-	seq_printf(m,i);
+	char buffer[sizeof(int) * 8  + 1]; // this is a string buffer we will put the string version of i, admittidly overkill
+	itoa(i, buffer, DECIMAL) 
+	seq_printf(m,buffer);
 	return 0;
 }
 
 static int __init proc_count_init(void)
 {
-	entry = proc_create_sinlge("count", 0, NULL, example)//creates a process of that name
+	//check if there is already an entry, if so removes it
+	if(!entry){
+		proc_remove(entry); //?? do i need this here too
+		entry = NULL;
+	}
+	
+	//creates a process of that name
+	entry = proc_create_single("count", 0, NULL, example);
+	
 	pr_info("proc_count: init\n");
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
-	proc_remove(entry);//how does it work
+	if(!entry){
+		proc_remove(entry);
+	}
 	pr_info("proc_count: exit\n");
 }
 
