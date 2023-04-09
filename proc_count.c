ca2c16dcd5d677ea5d7cddc43188d5d0c716eef8
morganmason0606 <morganmason0606@gmail.com>
2023-04-08 17:19:49 -0700

    first final draft, copying from what was written on the virtual machine
    could use more tests, but does pass given ones

--- proc_count.c
+++ proc_count.c
@@ -4,13 +4,15 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
-static int example(struct seq_file *m, struct task_struct *p){
+static struct proc_dir_entry *entry; 
+static int count_proc(struct seq_file *m, void *v){
 	
 
 	//int here assumes there will be less than UINT_MAX processes on a computer, this feels like a fair assumption
 	unsigned int i = 0; 
 	
 	//we use for_each_process(struct task_struct *p) to add to i for each process
+	struct task_struct *p;
 	for_each_process(p){
 		i++;
 	}
@@ -25,7 +27,7 @@ static int __init proc_count_init(void)
 	
 	
 	//creates a process named count permission 644 with no parent
-	static struct proc_dir_entry *entry = proc_create_single("count", 0644, NULL, example);
+	static struct proc_dir_entry *entry = proc_create_single("count", 0, NULL, count_proc);
 	
 	pr_info("proc_count: init\n");
 	return 0;
